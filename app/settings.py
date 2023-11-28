28037f831c6beb60d48622729dee9bba54909d13
Rodrigo Araujo <rodferaraujo@gmail.com>
2023-11-28 10:37:52 -0300

    feat: adjust timezone
    - Adjust settings timezone
    - Adjust gitignore

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ad+sw0g=tu9&(012_y=%#)3-02+^i#y4r1w3#kon$91a#@!a5s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
